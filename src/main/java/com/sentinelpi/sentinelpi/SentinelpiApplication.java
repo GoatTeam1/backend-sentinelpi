6133fa9172623af40332c76fcbf1abbcd0e24629
JazzoLopez <Jazzielrodriguezlopez@gmail.com>
2025-06-21 12:51:42 -0600

    first commit

---
+++ src/main/java/com/sentinelpi/sentinelpi/SentinelpiApplication.java
@@ -0,0 +1,13 @@
+package com.sentinelpi.sentinelpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sentinelpi/sentinelpi/SentinelpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinelpi.sentinelpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
