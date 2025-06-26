e2dc7aebccbce04bf2878767437986509a888664
JazzoLopez <Jazzielrodriguezlopez@gmail.com>
2025-06-26 16:54:02 -0600

    feat: add hierarchy order in security settings

--- src/main/java/com/sentinelpi/sentinelpi/config/SecurityConfig.java
+++ src/main/java/com/sentinelpi/sentinelpi/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.sentinelpi.sentinelpi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 //TODO: Debemos arreglar e implementar la seguridad de las rutas
 @Configuration
+@Order(1) // Orden de jerarquia
 public class SecurityConfig {
 
     @Bean
